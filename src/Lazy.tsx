1f8a5a69097f72bc4407e11c97969ee7798b2b61
Joseph Hailu <johailu@deloitte.ca>
2025-04-15 14:53:57 -0400

    trigger new hash

--- src/Lazy.tsx
+++ src/Lazy.tsx
@@ -1,7 +1,7 @@
 function Lazy({ count = 10 }: { count?: number }) {
   return (
     <div>
-      <p className="read-the-docs">Lazy loaded: {10 / count} </p>
+      <p className="read-the-docs">Lazy loaded: {11 / count} </p>
     </div>
   );
 }
