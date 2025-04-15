d295467ecd9028acca3ef7e82e3d0352d6c5060d
Joseph Hailu <johailu@deloitte.ca>
2025-04-15 14:26:46 -0400

    routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,20 @@
-import { StrictMode, Suspense } from "react";
+import { StrictMode, Suspense, lazy } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+
 import "./index.css";
 import App from "./App.tsx";
+const Lazy = lazy(() => import("./Lazy"));
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Suspense fallback={<div>...Loading</div>}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/lazy" element={<Lazy />} />
+        </Routes>
+      </BrowserRouter>
     </Suspense>
   </StrictMode>
 );
