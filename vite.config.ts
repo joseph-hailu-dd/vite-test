a09b1095a7496d31e7a3997fae9b5f4436e3437a
Joseph Hailu <johailu@deloitte.ca>
2025-04-15 15:00:12 -0400

    no hash

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name][extname]",
+        chunkFileNames: "assets/[name].js",
+        entryFileNames: "[name].js",
+      },
+    },
+  },
 });
