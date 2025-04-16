091500384d3eba1b9323fd31ad76873c5ded4a95
Joseph Hailu <johailu@deloitte.ca>
2025-04-16 10:24:48 -0400

    no lazy

--- src/Lazy.tsx
+++ src/Lazy.tsx
@@ -1,7 +1,7 @@
 function Lazy({ count = 10 }: { count?: number }) {
   return (
     <div>
-      <p className="read-the-docs">Lazy loaded: {111 / count} </p>
+      <p className="read-the-docs">No longer Lazy loaded: {111 / count} </p>
       <br />
       <br />
       <br />
