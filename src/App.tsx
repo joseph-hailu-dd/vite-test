4da60bab48ecfe4845e0938d9781aa298a53339f
Joseph Hailu <johailu@deloitte.ca>
2025-04-16 09:43:47 -0400

    update title

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + PWA update prompt</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 2)}>
           count is {count}
