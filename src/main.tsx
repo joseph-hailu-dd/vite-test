a85576ba9eff0758f12596e5d31b2f6a16071733
Joseph Hailu <johailu@deloitte.ca>
2025-04-16 10:15:38 -0400

    try no dynamic route

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
-import { StrictMode, Suspense, lazy } from "react";
+import {
+  StrictMode,
+  Suspense,
+  // lazy
+} from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 
 import "./index.css";
 import App from "./App.tsx";
 import ReloadPrompt from "./ReloadPrompt.tsx";
-const Lazy = lazy(() => import("./Lazy"));
+import Lazy from "./Lazy";
+// const Lazy = lazy(() => import("./Lazy"));
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
