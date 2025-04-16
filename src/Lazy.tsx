ebd57d0fa591be6ee341db6cc77813a021347064
Joseph Hailu <johailu@deloitte.ca>
2025-04-16 08:18:12 -0400

    test service worker prompt

--- src/Lazy.tsx
+++ src/Lazy.tsx
@@ -2,6 +2,10 @@ function Lazy({ count = 10 }: { count?: number }) {
   return (
     <div>
       <p className="read-the-docs">Lazy loaded: {111 / count} </p>
+      <br />
+      <br />
+      <br />
+      <p className="read-the-docs">Date: {new Date().toISOString()} </p>
     </div>
   );
 }
