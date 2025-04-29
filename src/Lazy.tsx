74c0ddb7ed899baf086a662e75c112879442ae12
Joseph Hailu <johailu@deloitte.ca>
2025-04-29 10:04:29 -0400

    go home

--- src/Lazy.tsx
+++ src/Lazy.tsx
@@ -1,6 +1,12 @@
+import { NavLink } from "react-router";
+
 function Lazy({ count = 10 }: { count?: number }) {
   return (
     <div>
+      <NavLink to="/">Home</NavLink>
+      <br />
+      <br />
+      <br />
       <p className="read-the-docs">No longer Lazy loaded: {111 / count} </p>
       <br />
       <br />
