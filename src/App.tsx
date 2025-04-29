53ed282ebc9e71fa495e1dec2ec9a9d89b089961
Joseph Hailu <johailu@deloitte.ca>
2025-04-29 09:45:31 -0400

    test

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React + Vite PWA update prompt</h1>
+      <h1>Test Vite + React + Vite PWA update prompt</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
