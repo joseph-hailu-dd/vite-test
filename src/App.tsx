7c84edbdfe4a2ca56652180f30671097ec6124ad
Joseph Hailu <johailu@deloitte.ca>
2025-04-28 22:13:00 -0400

    update count and heading

--- src/App.tsx
+++ src/App.tsx
@@ -17,9 +17,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React + PWA update prompt</h1>
+      <h1>Vite + React + Vite PWA update prompt</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 2)}>
+        <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
